2349b4dbc34d9d5e076ef91111744857206e767e
the_p <the_p@LAPTOP-77GCKEU2>
2022-05-04 12:05:20 +0200

    Este es el primer commit

---
+++ src/repaso/PruebaRepaso.java
@@ -0,0 +1,33 @@
+package repaso;
+
+import java.util.Scanner;
+
+public class PruebaRepaso {
+
+	public static void main(String[] args) {
+		int num1=0, num2=0;
+		Scanner ent =new Scanner(System.in);
+		System.out.println("Introduce un numero:");
+		num1=Integer.parseInt(ent.nextLine());
+		System.out.println("Introduce un numero:");
+		num2=Integer.parseInt(ent.nextLine());
+		
+		sumar(); //Laro has este método.
+		restar(); 
+		
+
+	}
+	
+	public static void sumar() {
+
+
+	}
+	
+	public static void restar() {
+
+
+
+	}
+	}
+	
+	
